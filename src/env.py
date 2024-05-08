741b6b8bc82b84c6d8f7b61350273a43e2c9b75b
SunPipe <g.intonti@studenti.unisa.it>
2024-05-08 12:31:19 +0200

    [FIX] Fixed gym env

--- src/env.py
+++ src/env.py
@@ -3,73 +3,77 @@ import numpy as np
 import time
 import pygame
 from agent import PIDController
-# env = gym.make('Pendulum-v1', g=9.81)
 
-# # Then we reset this environment
-# observation = env.reset()
-# # Observation and action space 
-# observed_space = env.observation_space
-# action_space = env.action_space
-# print("The observation space:{}" .format(observed_space))
-# print("The action space: {}".format(action_space))
+env = gym.make('Pendulum-v1', g=9.81, render_mode="human")
 
-# # Perform 10 random actions
-# for _ in range(10):
-#   # Take a random action
-#   action = env.action_space.sample()
-#   print("Action taken:", action)
+# Then we reset this environment
+observation = env.reset()
+# Observation and action space 
+# select render mode to human
+env.render()
 
-#   # Do this action in the environment and get
-#   # next_state, reward, done and info
-#   obs, reward, terminated, truncated, info = env.step(action)
-#   # If the game is done (in our case we land, crashed or timeout)
-#   if terminated or truncated:
-#       # Reset the environment
-#       print("Environment is reset")
-#       observation = env.reset()
+observed_space = env.observation_space
+action_space = env.action_space
+print("The observation space:{}" .format(observed_space))
+print("The action space: {}".format(action_space))
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((450, 450))
-clock = pygame.time.Clock()
-pygame.display.set_caption('Hello World!')
-pygame.display.flip()
+# Perform 10 random actions
+while True:
+  # Take a random action
+  action = env.action_space.sample()
+  print("Action taken:", action)
 
-env = gym.make('Pendulum-v1', g=9.81,render_mode="rgb_array")
+  # Do this action in the environment and get
+  # next_state, reward, done and info
+  obs, reward, terminated, truncated, info = env.step(action)
+  # If the game is done (in our case we land, crashed or timeout)
+  if terminated or truncated:
+      # Reset the environment
+      print("Environment is reset")
+      observation = env.reset()
 
-dt = env.unwrapped.dt # Time step of the simulation
+# pygame.init()
+# DISPLAYSURF = pygame.display.set_mode((450, 450))
+# clock = pygame.time.Clock()
+# pygame.display.set_caption('Hello World!')
+# pygame.display.flip()
+
+# env = gym.make('Pendulum-v1', g=9.81,render_mode="rgb_array")
+
+# dt = env.unwrapped.dt # Time step of the simulation
 
 # Choose appropriate PID gains (experimentation is key)
-controller = PIDController(kp=0.05, ki=0.03, kd=0.02)
+# controller = PIDController(kp=0.05, ki=0.03, kd=0.02)
 
-state = env.reset()
-image = env.render()
+# state = env.reset()
+# image = env.render()
 
 # Convert image to pygame surface
-image = pygame.surfarray.make_surface(image)
-DISPLAYSURF.blit(image, (0, 0))
-pygame.display.update()
+# image = pygame.surfarray.make_surface(image)
+# DISPLAYSURF.blit(image, (0, 0))
+# pygame.display.update()
 
 
-print("Initial state:", state)
-print("sin(theta):", state[0][0], "cos(theta):", state[0][1], "theta_dot:", state[0][2])
-terminated = truncated = False
+# print("Initial state:", state)
+# print("sin(theta):", state[0][0], "cos(theta):", state[0][1], "theta_dot:", state[0][2])
+# terminated = truncated = False
 
-id = 0
-while not terminated or not truncated:
-  # Get control action based on current state
-  action = controller.control(state, dt)
+# id = 0
+# while not terminated or not truncated:
+#   Get control action based on current state
+#   action = controller.control(state, dt)
 
-  # Take action and observe next state, reward, etc.
-  next_state, reward,terminated, truncated, info = env.step(action)
-  state = next_state
+#   Take action and observe next state, reward, etc.
+#   next_state, reward,terminated, truncated, info = env.step(action)
+#   state = next_state
 
-  time.sleep(0.01)
-  image = env.render()
-  # Convert image to pygame surface
-  image = pygame.surfarray.make_surface(image)
-  DISPLAYSURF.blit(image, (0, 0))
-  pygame.display.update()
+#   time.sleep(0.01)
+#   image = env.render()
+#   Convert image to pygame surface
+#   image = pygame.surfarray.make_surface(image)
+#   DISPLAYSURF.blit(image, (0, 0))
+#   pygame.display.update()
 
-  #id += 1
+#   id += 1
 
-env.close()
+# env.close()
