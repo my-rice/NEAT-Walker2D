453fea78f1aa5c666f8815e3b08ee77e178866f3
giovannintonti <g.intonti@studenti.unisa.it>
2024-05-14 17:05:33 +0200

    [ADD] MUltiple agent

--- main.py
+++ main.py
@@ -2,38 +2,77 @@ from src.env import Environment, AvailableEnvironments
 from src.legged_robot import LeggedRobot, AvailableAgents
 import hydra
 from omegaconf import MISSING, OmegaConf
+import neat
+import pickle
+from src.legged_robot_app import LeggedRobotApp
 
-# Create the environment
+ENV_NAME = None
+AGENT_NAME = None
+
+def evolutionary_driver(n, neat_config):
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         neat_config)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+
+    # Run until we achive n.
+    winner = p.run(eval_genomes, n=n) 
+
+    # dump
+    pickle.dump(winner, open('winner.pkl', 'wb'))
+
+    plot_winner(file_winner_net='winner.pkl', config=config)
+
+def plot_winner(file_winner_net, config):
+    with open(file_winner_net, 'rb') as f:
+        winner = pickle.load(f)
+    print('\nBest genome:\n{!s}'.format(winner))
+
+def eval_genomes(genomes, config):
+    
+    # Play game and get results
+    idx,genomes = zip(*genomes)
+    
+    legged_Bio = LeggedRobotApp(genomes, config, ENV_NAME, AGENT_NAME)
+    legged_Bio.play()
+    results = legged_Bio.crash_info
+    
+    # Calculate fitness and top score
+    top_score = 0
+    for result, genomes in results:
+
+        score = 3
+        distance = 2
+        energy = 1
+
+        fitness = score*3000 + 0.2*distance - energy*1.5
+        genomes.fitness = -1 if fitness == 0 else fitness
+        
+
+    # print score
+    print('The top score was:', top_score)
 
 @hydra.main(config_path=".", config_name="config", version_base="1.2")
 def main(cfg):
+    global ENV_NAME, AGENT_NAME
     missing_keys: set[str] = OmegaConf.missing_keys(cfg)
     if missing_keys:
         raise RuntimeError(f"Got missing keys in config:\n{missing_keys}")
+    assert cfg.max_episodes > 0
+    assert cfg.generations > 0
+    assert cfg.render is True or cfg.render is False
     assert AvailableEnvironments.has_value(cfg.env_name)
     assert AvailableAgents.has_value(cfg.agent_name)
-    assert cfg.max_episodes > 0
-    assert cfg.render is True or cfg.render is False
-    env = Environment(cfg.env_name)
-        
-    observation_space_dim = env.get_observation_space()
-    action_space_dim = env.get_action_space()
-    # Create the legged robot agent
-    agent = LeggedRobot(observation_space_dim, action_space_dim)
-    print(agent)
-    # Main loop
-    for i in range(cfg.max_episodes):
-        observation = env.reset()
-        done = False
-        while not done:
-            action = agent.compute_action(observation)
-            observation, reward, done, info = env.step(action)
-
-            print("Fitness: ", env.fitness())
-            #agent.update_agent(env.fitness())
-            if(cfg.render):
-                env.render()
-    env.close()
+    assert cfg.neat_config is not None
+    ENV_NAME = cfg.env_name
+    AGENT_NAME = cfg.agent_name
+    evolutionary_driver( n=cfg.generations, neat_config=cfg.neat_config) # check if is a real config
 
 if __name__ == "__main__":
     main()
--- src/legged_robot.py
+++ src/legged_robot.py
@@ -13,10 +13,12 @@ class AvailableAgents(Enum):
 
 class LeggedRobot(Agent):
 
-    def __init__(self, observation_space_dim, action_space_dim) -> None:
+    def __init__(self, observation_space_dim, action_space_dim, genome, config) -> None: # teoricamente qua dovrebbe cambiare in base all'agente che passiamo
         # We need to initialize the neural network with the given observation space and action space
         self.observation_space_dim = observation_space_dim
-        self.action_space_dim = action_space_dim        
+        self.action_space_dim = action_space_dim     
+        print("Observation space dim: ", self.observation_space_dim) 
+        print("Action space dim: ", self.action_space_dim)   
     
     def compute_action(self, observation: np.ndarray) -> np.ndarray:
         # Compute the action based on the network 
