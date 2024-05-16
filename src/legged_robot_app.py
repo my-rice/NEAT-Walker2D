b487538ae430b15a9d23428ce9b2dfa8ba1ddce9
giovannintonti <g.intonti@studenti.unisa.it>
2024-05-16 11:00:57 +0200

    [ADD] Aggiornamenti vari

--- src/legged_robot_app.py
+++ src/legged_robot_app.py
@@ -11,7 +11,7 @@ class LeggedRobotApp(object):
 
     def __init__(self, genomes, config, env_name="Walker2d-v4", agent_name="LeggedRobot"):
         print(env_name)
-        self.envs = []
+        self.experiments = []
         self.start=0
         self.score = 0
         self.crash_info = []
@@ -23,25 +23,31 @@ class LeggedRobotApp(object):
             else:
                 env=Environment(env_name, "rgb_array")
             env.set_index(index)
-            observation = env.reset()
-            self.envs.append([env, observation])
+            env.reset()
             legged_robot = LeggedRobot(env.observation_space.shape[0],env.action_space.shape[0],genome,config)
-            self.robots.append(legged_robot)
+            self.experiments.append([env,legged_robot,0.0])
 
+    def set_fitness(self, fitness, index):
+        self.experiments[index][2] = fitness
 
     def on_loop(self):
         
-        for index, env in enumerate(self.envs):
-            if(self.start==0):
-                env[1]=env[1][0]
-            action = self.robots[index].compute_action(env[1])
-            env[1], _, done , _=env[0].step(action)
-
+        for count, experiment in enumerate(self.experiments):
+            env = experiment[0]
+            agent = experiment[1]
+            fitness = experiment[2]
+            # if(self.start==0):
+            #     env[1]=env[1][0]
+            action = agent.compute_action(env.get_current_observation())
+            _, _, done , _=env.step(action)
+            fitness = fitness + env.fitness()
+            self.set_fitness(fitness, count)
             if done:
-                self.crash_info.append((self.robots[index]))
-                del self.envs[index]
-                print("The environment", env[0].get_index(), "has been removed")
-                if(len(self.envs)==0):
+                self.crash_info.append((agent.get_genome(), fitness))
+                del self.experiments[count]
+                #print("The environment", env.get_index(), "has been removed")
+                env.close()
+                if(len(self.experiments)==0):
                     print("All the environments are dead")
                     return True
             
